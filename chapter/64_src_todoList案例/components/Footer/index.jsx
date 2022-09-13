e4ded33792f5d56e3a1ef0954d368cde12774e47
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-13 16:20:16 +0800

    chapter 64

---
+++ chapter/64_src_todoList案例/components/Footer/index.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import "./index.css"
+
+export default class Footer extends Component {
+
+    // 全選 checkbox 的回調
+    handleCheckAll = (event) => {
+        this.props.checkAllTodo(event.target.checked)
+    }
+
+    // 清除已完成任務的回調
+    handleClearAllDone = () => {
+        if (window.confirm("確定清除嗎？")) {
+            this.props.clearAllDone()
+        }
+    }
+
+    render() {
+        const { todos } = this.props
+        // 已完成的個數
+        const doneCount = todos.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0)
+        // 總數
+        const total = todos.length;
+        return (
+            <div className="todo-footer">
+                <label>
+                    <input type="checkbox" onChange={this.handleCheckAll} checked={doneCount === total && total !== 0 ? true : false} />
+                </label>
+                <span>
+                    <span>已完成{doneCount}</span>/全部{total}
+                </span>
+                <button onClick={this.handleClearAllDone} className="btn btn-danger">清除已完成任務</button>
+            </div>
+        )
+    }
+}
