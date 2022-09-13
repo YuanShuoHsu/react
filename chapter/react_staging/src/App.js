0c5f1573a74ca41cccc77f072472bc76107c8ab9
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-13 13:50:59 +0800

    chapter 58

--- chapter/react_staging/src/App.js
+++ chapter/react_staging/src/App.js
@@ -5,12 +5,34 @@ import Footer from './components/Footer'
 import "./App.css"
 
 export default class App extends Component {
+
+    // 初始化狀態
+    state = {
+        todos: [
+            { id: "001", name: "吃飯", done: true },
+            { id: "002", name: "睡覺", done: true },
+            { id: "003", name: "打代碼", done: false },
+            { id: "004", name: "逛街", done: true }
+        ]
+    }
+
+    // addTodo 用於添加一個 todo，接收的參數是 todo 對象
+    addTodo = (todoObj) => {
+        // 獲取原 todos
+        const { todos } = this.state
+        // 追加一個 todo
+        const newTodos = [todoObj, ...todos]
+        // 更新狀態
+        this.setState({ todos: newTodos })
+    }
+
     render() {
+        const { todos } = this.state
         return (
             <div className="todo-container">
                 <div className="todo-wrap">
-                    <Header />
-                    <List />
+                    <Header addTodo={this.addTodo} />
+                    <List todos={todos} />
                     <Footer />
                 </div>
             </div>
--- chapter/react_staging/src/components/Header/index.jsx
+++ chapter/react_staging/src/components/Header/index.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import { nanoid } from "nanoid"
 import "./index.css"
 
 export default class Header extends Component {
+
+    handleKeyUp = (event) => {
+        // 解構賦值 keyCode, target
+        const { keyCode, target } = event
+        // 判斷是否是回車按鍵
+        if (keyCode !== 13) return
+        // 添加的 todo 名字不能為空
+        if (target.value.trim() === "") {
+            alert("輸入不能為空")
+            return
+        }
+        // 準備好一個 todo 對象
+        const todoObj = { id: nanoid(), name: target.value, done: false }
+        // 將 todoObj 傳遞給 App
+        this.props.addTodo(todoObj);
+        // 清空輸入
+        target.value = ""
+    }
+
     render() {
         return (
             <div className="todo-header">
-                <input type="text" placeholder='請輸入你的任務名稱，按回車鍵確認' />
+                <input onKeyUp={this.handleKeyUp} type="text" placeholder='請輸入你的任務名稱，按回車鍵確認' />
             </div>
         )
     }
