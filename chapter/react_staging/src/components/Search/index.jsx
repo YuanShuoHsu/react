655fa22ebb618770f119b6c5d91827a20908c7b0
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-14 16:56:32 +0800

    chapter 68

--- chapter/react_staging/src/components/Search/index.jsx
+++ chapter/react_staging/src/components/Search/index.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class Search extends Component {
+
+  search = () => {
+    // 獲取用戶的輸入(連續解構賦值 + 重命名)
+    const { keyWordElement: { value: keyWord } } = this
+    console.log(keyWord)
+    // 發送網路請求
+    axios.get(`/api1/search/users?q=${keyWord}`).then(
+      response => { console.log("成功了", response.data) },
+      error => { console.log("失敗了", error) }
+    )
+  }
+
   render() {
     return (
       <section className="jumbotron">
-        <h3 className="jumbotron-heading">Search Github Users</h3>
+        <h3 className="jumbotron-heading">搜索 github 用戶</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="輸入關鍵詞點擊搜索" />&nbsp;
+          <button onClick={this.search}>搜索</button>
         </div>
       </section>
     )
---
+++ chapter/react_staging/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware('/api1', { // 遇見 api1 前綴的請求，就會觸發該代理配置
+            target: 'http://localhost:5000', // 請求轉發給誰
+            changeOrigin: true, // 控制服務器收到的請求頭中 Host 字段的值
+            pathRewrite: { '^/api1': '' } // 重寫請求路徑(必須)
+        })
+    )
+}
