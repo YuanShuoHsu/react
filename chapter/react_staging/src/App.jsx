6e967039b7ecc5bdd2d8ffce4158840239f2d17a
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-14 15:53:49 +0800

    chapter 67

--- chapter/react_staging/src/App.jsx
+++ chapter/react_staging/src/App.jsx
@@ -1,27 +1,13 @@
 import React, { Component } from 'react'
-import axios from "axios"
+import Search from './components/Search'
+import List from './components/List'
 
 export default class App extends Component {
-
-  getStudentData = () => {
-    axios.get("http://localhost:3000/api1/students").then(
-      reponse => { console.log("成功了", reponse.data) },
-      error => { console.log("失敗了", error) }
-    )
-  }
-
-  getCarData = () => {
-    axios.get("http://localhost:3000/api2/cars").then(
-      reponse => { console.log("成功了", reponse.data) },
-      error => { console.log("失敗了", error) }
-    )
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.getStudentData}>點我獲取學生數據</button>
-        <button onClick={this.getCarData}>點我獲取汽車數據</button>
+      <div className="container">
+        <Search />
+        <List />
       </div>
     )
   }
