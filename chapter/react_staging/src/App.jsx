e458b577afadf16a011545aa6152c13d7acc7aad
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-14 14:55:45 +0800

    chapter 65

--- chapter/react_staging/src/App.jsx
+++ chapter/react_staging/src/App.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class App extends Component {
+
+  getStudentData = () => {
+    axios.get("http://localhost:3000/students").then(
+      reponse => { console.log("成功了", reponse.data) },
+      error => { console.log("失敗了", error) }
+    )
+  }
+
+  // getCarData = () => {
+  //   axios.get("http://localhost:3000/api2/cars").then(
+  //     reponse => { console.log("成功了", reponse.data) },
+  //     error => { console.log("失敗了", error) }
+  //   )
+  // }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        <button onClick={this.getStudentData}>點我獲取學生數據</button>
+        {/* <button onClick={this.getCarData}>點我獲取汽車數據</button> */}
+      </div>
     )
   }
 }
