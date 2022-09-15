25d86e01b42a55dfd57153f57c51f360e1660dc3
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-15 14:08:33 +0800

    chapter 69

--- chapter/react_staging/src/App.jsx
+++ chapter/react_staging/src/App.jsx
@@ -3,19 +3,24 @@ import Search from './components/Search'
 import List from './components/List'
 
 export default class App extends Component {
-  state = {
-    users: [] // 初始化狀態，users 初始值為數組
+
+  state = { // 初始化狀態
+    users: [], // users 初始值為數組
+    isFirst: true, // 是否為第一次打開頁面
+    isLoading: false, // 標識是否處於加載中
+    err: "", // 存儲請求相關錯誤信息
   }
 
-  saveUsers = (users) => {
-    this.setState({ users })
+  // 更新 App 的 state
+  updateAppState = (stateObj) => {
+    this.setState(stateObj)
   }
+
   render() {
-    const { users } = this.state
     return (
       <div className="container">
-        <Search saveUsers={this.saveUsers} />
-        <List users={users} />
+        <Search updateAppState={this.updateAppState} />
+        <List {...this.state} />
       </div>
     )
   }
--- chapter/react_staging/src/components/Search/index.jsx
+++ chapter/react_staging/src/components/Search/index.jsx
@@ -6,19 +6,25 @@ export default class Search extends Component {
   search = () => {
     // 獲取用戶的輸入(連續解構賦值 + 重命名)
     const { keyWordElement: { value: keyWord } } = this
-    console.log(keyWord)
+    // 發送請求前通知 App 更新狀態
+    this.props.updateAppState({ isFirst: false, isLoading: true })
     // 發送網路請求
     axios.get(`/api1/search/users?q=${keyWord}`).then(
       response => {
-        this.props.saveUsers(response.data.items)
+        // 請求成功後通知 App 更新狀態
+        this.props.updateAppState({ isLoading: false, users: response.data.items })
+        // this.props.saveUsers(response.data.items)
       },
-      error => { console.log("失敗了", error) }
+      error => {
+        // 請求失敗後通知 App 更新狀態
+        this.props.updateAppState({ isLoading: false, err: error.message })
+      }
     )
   }
 
   render() {
     return (
-      <section className="jumbotron">
+      <section className="jumbotron" >
         <h3 className="jumbotron-heading">搜索 github 用戶</h3>
         <div>
           <input ref={c => this.keyWordElement = c} type="text" placeholder="輸入關鍵詞點擊搜索" />&nbsp;
