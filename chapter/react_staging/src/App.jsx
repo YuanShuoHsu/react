a155ba636aac4537179962a1884d0a103f6c35f1
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-15 12:13:40 +0800

    chapter 70

--- chapter/react_staging/src/App.jsx
+++ chapter/react_staging/src/App.jsx
@@ -3,11 +3,19 @@ import Search from './components/Search'
 import List from './components/List'
 
 export default class App extends Component {
+  state = {
+    users: [] // 初始化狀態，users 初始值為數組
+  }
+
+  saveUsers = (users) => {
+    this.setState({ users })
+  }
   render() {
+    const { users } = this.state
     return (
       <div className="container">
-        <Search />
-        <List />
+        <Search saveUsers={this.saveUsers} />
+        <List users={users} />
       </div>
     )
   }
--- chapter/react_staging/src/components/Search/index.jsx
+++ chapter/react_staging/src/components/Search/index.jsx
@@ -9,7 +9,9 @@ export default class Search extends Component {
     console.log(keyWord)
     // 發送網路請求
     axios.get(`/api1/search/users?q=${keyWord}`).then(
-      response => { console.log("成功了", response.data) },
+      response => {
+        this.props.saveUsers(response.data.items)
+      },
       error => { console.log("失敗了", error) }
     )
   }
