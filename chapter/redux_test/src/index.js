94e1197e70e23337ea01e003bbc5ee08da144422
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2022-09-25 16:25:23 +0800

    chapter 107

--- chapter/redux_test/src/index.js
+++ chapter/redux_test/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 import store from "./redux/store"
+import { Provider } from "react-redux"
 
-ReactDOM.render(<App />, document.getElementById("root"))
-
-// 監測 redux 中狀態的改變，如 redux 的狀態發生了改變，那麼重新渲染 App 組件
-store.subscribe(() => {
-    ReactDOM.render(<App />, document.getElementById("root"))
-})
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+)
